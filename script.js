d3df45cdca63bfec088f154a146d1008ea5bb2b7
Dain Im <dain98@gmail.com>
2021-08-12 17:35:08 -0400

    I forgot to update this

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 let addlink = document.getElementById("addlink");
+let loading = document.getElementById('loading');
+var button = document.getElementById("submit");
 var i = 0;
-var form = 2;
-$(function() {
-    $('#addlink').click(function(){
-        var newDiv = $('<div class="form__group field"><input type="input" class="form__field" placeholder="Link #1" name="link1" id="link1" value="" required /><label for="link1" class="form__label">MP Link #' + form + '</label></div>');
-      //newDiv.style.background = "#000";
-      $('#form').append(newDiv);
-      form++;
-    });
-    });
-// addlink.addEventListener("click", function() {
-//     var original = document.getElementById('form');
-//     var clone = original.cloneNode(true);
-//     clone.id = "form" + ++i;
-//     original.parentNode.appendChild(clone);
-// //   let form = document.getElementById("form");
-// //   let clone = form.firstElementChild.cloneNode(true);
-// //   form.appendChild(clone);
-// });
+var forms = 2;
 
-function submit(){
-    var form = document.querySelectorAll("#form input");
-    var tbodyRef = document.getElementById('res').getElementsByTagName('tbody')[0];
-    for (var i = 0;i < form.length;i++){
-        
-        var newRow = tbodyRef.insertRow();
-        var newCell = newRow.insertCell();
-        var newText = document.createTextNode(form[i].value); // PLAYER NAME
-        newCell.appendChild(newText);
-        var newCell = newRow.insertCell();
-        var newText = document.createTextNode("test"); // COST VALUE
-        newCell.appendChild(newText);
-        var newCell = newRow.insertCell();
-        var newText = document.createTextNode("test2"); // # OF MAPS PLAYED
-        newCell.appendChild(newText);
+
+async function submit(){
+    button.classList.add("disabled");
+    var fields = document.getElementsByClassName("validate");
+    var matches = "";
+    var warmups = "";
+    for (var i = 0;i < fields.length;i++) {
+        if (i % 2) {
+            if (fields[i].value == "") {
+                warmups += "2,";
+            } else {
+                warmups += fields[i].value + ",";
+            }
+        } else {
+            var tempMatch = fields[i].value.split("/");
+            matches += tempMatch.pop() + ",";
+        }
     }
+    var tbodyRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+    const response = await fetch('https://dain.cafe/matches/' + matches.replace(/,\s*$/, "") + '&' + warmups.replace(/,\s*$/, ""));
+    const costs = await response.json();
+    var new_tbody = document.createElement('tbody');
+    costs.forEach(function (player, index) {
+        var newRow = new_tbody.insertRow();
+        var newCell = newRow.insertCell();
+        var newText = document.createTextNode(player[0]); // PLAYER NAME
+        newCell.appendChild(newText);
+        var newCell = newRow.insertCell();
+        var newText = document.createTextNode(player[1]); // COST VALUE
+        newCell.appendChild(newText);
+    });
+    tbodyRef.parentNode.replaceChild(new_tbody,tbodyRef);
+    button.classList.remove("disabled");
 }
 
-function actionToggle() {
-    const action = document.querySelector('.action');
-    action.classList.toggle('active')
-  }
+function addField() {
+    const div = document.createElement('div');
+    const form = document.getElementById('form');
+    console.log(form);
+    div.className = 'row';
+    div.innerHTML = `<div class="input-field col s9"><input id="mplink` + forms + `" type="text" class="validate white-text"><label for="mplink` + forms + `">MP Link #` + forms + `</label></div><div class="input-field col s3"><input id="warmup` + forms + `" type="text" class="validate white-text"><label for="warmup` + forms + `">Warmups</label></div>`
+    form.appendChild(div);
+    forms++;
+}
